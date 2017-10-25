2c955af84e3513a25d6cb80fa166700b6435dacb
digitopoi <hubo85@gmail.com>
2017-10-25 18:55:35 -0500

    Initial commit

---
+++ WineStore/Models/Wine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WineStore.Models
+{
+    public class Wine
+    {
+        public int ProductID { get; set; }
+        public string Winery { get; set; }
+        public DateTime Year { get; set; }
+        public string Region { get; set; }
+        public string Grape { get; set; }
+        public string Color { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ WineStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WineStore.Startup))]
+namespace WineStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
